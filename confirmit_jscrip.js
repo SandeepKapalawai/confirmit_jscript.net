ac9f5620c1ef671b90686cb000917a16551077a7
Trivender Singh <trivenderghangas87@gmail.com>
2017-06-30 13:36:53 +0530

    added function "only" and "arrDiff"

---
+++ confirmit_jscrip.js
@@ -0,0 +1,49 @@
+/**
+* @param string qid : Question id of single or multi select question
+* @param array codes : Array of codes to test.
+* @return bool : Returns true if only subset of tested codes is selected (no other code selected).
+*/
+function only(qid, codes){
+	var selectedOnlyCodes = false;
+	var selectedOtherCodes = false;
+	var form = f(qid);
+	var dv = form.domainValues();
+	var otherCodes = arrDiff(dv,codes);
+	for(var i=0;i<codes.length;i++){
+		var thisc = codes[i];
+		if(form.any(thisc)){
+			selectedOnlyCodes = true;
+		}
+	}
+	for(var i=0;i<otherCodes.length;i++){
+		var thisc = otherCodes[i];
+		if(form.any(thisc)){
+			selectedOtherCodes = true;
+		}
+	}	
+return 	selectedOnlyCodes && !selectedOtherCodes;
+}
+
+
+/*******************array utilities*******************/
+/**
+Remove arr2 elements from arr1.
+* @param array arr1 : Array from which elements will be removed.
+* @param array arr2 : Array of elements that needs to be removed from arr1.
+* @return array : arr1 after removing arr2 elements .
+*/
+function arrDiff(arr1,arr2){
+	for(var i=0;i<arr2.length;i++){
+		var itemToRemove = arr2[i];	
+		var j = 0;
+		while (j < arr1.length){
+			if (arr1[j] == itemToRemove) {			
+				arr1.splice(j, 1);	
+			} 
+			else{
+				j++; 
+			}
+		}	
+	}
+return arr1;	
+}
